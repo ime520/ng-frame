6ce112156337123a04f12a0dd86cec6e1b0d1cb9
lsz <liusz@ddd.online>
2020-04-27 17:36:16 +0800

    angular material引入

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import Home from '../views/home/home.component';
+import About from '../views/about/about.component';
+import Login from '../views/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  { path: 'about', component: About },
+  { path: 'login', component: Login },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
