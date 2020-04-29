5dca80b288246d85dd701eed9c39032a1e54999f
lsz <1038747706@qq.com>
2020-04-29 22:29:37 +0800

    布局搭建

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
 
 import { HomeModule } from './home/home.module';
 import { LoginModule } from './login/login.module';
-import { LayoutMainComponent } from './layout-main/layout-main.component';
 
 import { LayoutMainModule } from './layout-main/layout-main.module';
 
 @NgModule({
-  declarations: [AppComponent, LayoutMainComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,9 +21,6 @@ import { LayoutMainModule } from './layout-main/layout-main.module';
     LayoutMainModule,
     HomeModule,
     LoginModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
