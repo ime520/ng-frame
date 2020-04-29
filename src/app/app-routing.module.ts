acdd52aeb0860c35fcf0cc5e1004ec4dd1a4f3ba
lsz <1038747706@qq.com>
2020-04-29 21:37:31 +0800

    布局调试

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LayoutMainComponent } from './layout-main/layout-main.component';
+
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
@@ -8,10 +10,14 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: LayoutMainComponent,
+    children: [
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'login', component: LoginComponent },
+    ],
   },
-  { path: 'about', component: AboutComponent },
-  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 import { HomeModule } from './home/home.module';
 import { LoginModule } from './login/login.module';
+import { LayoutMainComponent } from './layout-main/layout-main.component';
+
+import { LayoutMainModule } from './layout-main/layout-main.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LayoutMainComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    LayoutMainModule,
     HomeModule,
     LoginModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
