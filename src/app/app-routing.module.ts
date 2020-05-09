bd9dd29e51ab710830a8441d472f8fef63e66dd5
lsz <liusz@ddd.online>
2020-05-09 16:09:40 +0800

    修改路由的架构

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LayoutMainComponent } from './layout-main/layout-main.component';
 import { LoginComponent } from './login/login.component';
+import { BlogComponent } from './blog/blog.component';
+import { DemoComponent } from './demo/demo.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'login', component: LoginComponent }];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutMainComponent,
+    children: [
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'blog', component: BlogComponent },
+      { path: 'demo', component: DemoComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layout-main/layout-main.module.ts
+++ src/app/layout-main/layout-main.module.ts
@@ -1,33 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutMainComponent } from './layout-main.component';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
-import { BlogComponent } from '../blog/blog.component';
-import { DemoComponent } from '../demo/demo.component';
-import { HomeComponent } from '../home/home.component';
-
 import { BlogModule } from '../blog/blog.module';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: LayoutMainComponent,
-    children: [
-      { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'blog', component: BlogComponent },
-      { path: 'demo', component: DemoComponent },
-    ],
-  },
-];
 @NgModule({
   declarations: [LayoutMainComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes),
+    RouterModule,
     MatToolbarModule,
     MatButtonModule,
     BlogModule,
