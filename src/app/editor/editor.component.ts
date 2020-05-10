d06b0afca2eac5034dc54413478ee12cedd8ee68
lsz <1038747706@qq.com>
2020-05-10 12:19:10 +0800

    集成富文本编辑器

---
+++ src/app/editor/editor.component.ts
@@ -0,0 +1,32 @@
+import {
+  Component,
+  ElementRef,
+  Renderer2,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import * as wangEditor from '../../../node_modules/wangeditor/release/wangEditor.js';
+
+@Component({
+  selector: 'editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.styl'],
+})
+export class EditorComponent {
+  private editor: any;
+  @Output() onPostData = new EventEmitter();
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    let editordom = this.el.nativeElement.querySelector('#editorElem');
+    this.editor = new wangEditor(editordom);
+    this.editor.customConfig.uploadImgShowBase64 = true;
+    this.editor.create();
+  }
+
+  clickHandle(): any {
+    let data = this.editor.txt.text();
+    return data;
+  }
+}
--- src/app/pub-article/pub-article.module.ts
+++ src/app/pub-article/pub-article.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PubArticleComponent } from './pub-article.component';
-
-
+import { EditorComponent } from '../editor/editor.component';
 
 @NgModule({
-  declarations: [PubArticleComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PubArticleComponent, EditorComponent],
+  imports: [CommonModule],
 })
-export class PubArticleModule { }
+export class PubArticleModule {}
